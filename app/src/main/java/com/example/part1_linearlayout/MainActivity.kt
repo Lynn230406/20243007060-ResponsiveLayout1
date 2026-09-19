f1342dbb54cd2ee626c49197dadedeaae78c7d80
Lynn230406 <15566314377@163.com>
2026-09-19 20:52:09 +0800

    Bind references to buttons and text view
    Declare variables and find views by id with explicit type for findViewById. Prepare for click listeners.

--- app/src/main/java/com/example/part1_linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/part1_linearlayout/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.part1_linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnChange: Button
+    private lateinit var btnCancel: Button
+    private lateinit var tvBlackText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_constraint)
 
+        btnChange = findViewById<Button>(R.id.btn_change)
+        btnCancel = findViewById<Button>(R.id.btn_cancel)
+        tvBlackText = findViewById<TextView>(R.id.tv_black_text)
 
         }
     }
-}
